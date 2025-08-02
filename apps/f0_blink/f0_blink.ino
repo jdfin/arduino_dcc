0707210760a13df02dfd13d4f85d96bcf89af067
John Finley <john.finley@gmail.com>
2025-08-01 20:20:28 -0500

    added apps/f0_blink

---
+++ apps/f0_blink/f0_blink.ino
@@ -0,0 +1,63 @@
+#include <Arduino.h>
+
+#include "xassert.h"
+#include "sys_led.h"
+
+#include "dcc_throttle.h"
+#include "dcc_command.h"
+
+
+static const int sig_gpio = 17;
+static const int pwr_gpio = 16;
+
+static DccAdc adc(-1);
+static DccCommand command(sig_gpio, pwr_gpio, adc);
+static DccThrottle *throttle = nullptr;
+
+
+void setup()
+{
+    Serial.begin(115200);
+
+    SysLed::begin();
+#if 0
+    SysLed::pattern(50, 950); // indicates "waiting for serial"
+
+    while (!Serial)
+        SysLed::loop();
+
+    SysLed::off();
+#endif
+
+    Serial.printf("\n");
+    Serial.printf("f0_blink\n");
+    Serial.printf("\n");
+
+    throttle = command.create_throttle();
+    throttle->address(1734);
+
+    command.mode_ops();
+}
+
+
+void loop()
+{
+    command.loop();
+
+    uint32_t now_ms = millis();
+
+    const uint32_t blink_hz = 2;
+    const uint32_t half_ms = 1000 / blink_hz / 2;
+
+    static uint32_t next_ms = now_ms + half_ms;
+    static bool f0 = false;
+
+    if (now_ms < next_ms)
+        return;
+
+    next_ms += half_ms;
+
+    f0 = !f0;
+
+    throttle->function(0, f0);
+}
